7838e2f418f24f4f77a897c005f7e2e306a91d73
Jaya Neem <jaya.neem@gmail.com>
2026-01-04 01:46:11 -0500

    Added interactive features

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,23 +1,43 @@
 const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
+const resultDiv = document.getElementById('result');
 function searchCondition() {
     const input = document.getElementById('travelInput').value.toLowerCase();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
-    fetch('health_analysis.json')
+    fetch('travel_recommendation_api.json')
       .then(response => response.json())
       .then(data => {
-        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
-        if (condition) {
-          const symptoms = condition.symptoms.join(', ');
-          const prevention = condition.prevention.join(', ');
-          const treatment = condition.treatment;
-          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
-          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        let recommendations = [];
+        if(input == "beach" || input == "beaches") {
+            recommendations.push(...data.beaches);
+        } else if(input == "temple" || input == "temples") {
+            recommendations.push(...data.temples);
+        } else if(input == "country" || input == "countries") {
+            data.countries.forEach(country => recommendations.push(...country.cities));
+            // recommendations.push(...data.countries.flatMap(country => country.cities));
+        }
+        if (recommendations) {
+            recommendations.forEach((recommendation) => {
+                const div = document.createElement("div");
+                const img = document.createElement("img");
+                const h2 = document.createElement("h2");
+                const p = document.createElement("p");
+                const button = document.createElement("button");
+                img.setAttribute("src", recommendation.imageUrl);
+                img.style.width = "100%";
+                h2.textContent = recommendation.name;
+                p.innerText = recommendation.description;
+                button.innerText = "Visit";
+                div.setAttribute('style', 'background-color: white;');
+                div.appendChild(img);
+                div.appendChild(h2);
+                div.appendChild(p);
+                div.appendChild(button);
+                resultDiv.appendChild(div);
+            });
         } else {
-          resultDiv.innerHTML = 'Condition not found.';
+          resultDiv.innerHTML = 'Recommendations not found.';
         }
       })
       .catch(error => {
@@ -25,4 +45,10 @@ function searchCondition() {
         resultDiv.innerHTML = 'An error occurred while fetching data.';
       });
   }
+  function clearResult() {
+    console.log("clear functionality called");
+    document.getElementById('travelInput').value = '';
+    resultDiv.innerHTML = "";
+  }
 btnSearch.addEventListener('click', searchCondition);
+btnClear.addEventListener('click', clearResult);
